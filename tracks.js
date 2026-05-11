dd5a5ef613e5dbf07044c2e4330160d42b1baf8b
tim-hy <txh816@alumni.bham.ac.uk>
2026-05-11 17:04:13 +0100

    Add tracks.js

---
+++ tracks.js
@@ -0,0 +1,13 @@
+var tracks = [
+    "https://tidal.com/track/488529279/u", 
+    "https://tidal.com/track/294950789/u", 
+    "https://tidal.com/track/128135751/u",
+    "https://tidal.com/track/404642060/u", 
+    "https://tidal.com/track/171117469/u", 
+    "https://tidal.com/track/386075266/u", 
+    "https://tidal.com/track/93764888/u", 
+    "https://tidal.com/track/455713360/u", 
+    "https://tidal.com/track/284620037/u", 
+    "https://tidal.com/track/427615333/u", 
+    "https://tidal.com/track/470378713/u"
+];
